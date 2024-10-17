ce222ea2db7700c973d026cfc05b00ac24334a52
chienabc <chien97666@gmail.com>
2024-10-17 15:06:10 +0700

    Initial Commit

---
+++ HP2-LESSON1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HP2-LESSON1
+//
+//  Created by CHIEN on 17/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
